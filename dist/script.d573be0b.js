2373eede217aa76c269f0dc50cf0cad6f892126e
Jason Pereira <jasonpereira@Jasons-MacBook-Pro.local>
2021-04-15 00:24:39 -0300

    alteracao logo

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -322,7 +322,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57580" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58482" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
